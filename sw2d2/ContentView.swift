b3443d817901b2aa8f7c747262d72f7449c753bc
Naif Ghannam <dhdfaui@gmail.com>
2025-03-10 12:08:34 +0300

    Initial Commit

---
+++ sw2d2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sw2d2
+//
+//  Created by Mac on 10/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sw2d2/sw2d2App.swift
@@ -0,0 +1,17 @@
+//
+//  sw2d2App.swift
+//  sw2d2
+//
+//  Created by Mac on 10/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct sw2d2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
